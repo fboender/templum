270529b33c6f039b7135e07d7bfb7130911928c1
todsah <todsah@76abf1b1-d79f-4ac6-b6dc-c8f82f358936>
2009-04-04 11:46:34 +0000

    Userguide build rule added.
    git-svn-id: https://svn.electricmonk.nl/svn/templum/trunk@20 76abf1b1-d79f-4ac6-b6dc-c8f82f358936

---
+++ build-rules/docs/userguide/b-run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp examples/all_syntax/view/account/list.tpl docs/userguide/
+cp examples/all_syntax/account_list.php docs/userguide/
+
+cd docs/userguide/
+asciidoc -a theme=classy -a toc -a toclevels=3 -a numbered userguide.txt
---
+++ build-rules/docs/userguide/c-run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm docs/userguide/userguide.html
+rm docs/userguide/list.tpl
+rm docs/userguide/account_list.php
+
