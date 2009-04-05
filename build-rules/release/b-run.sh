60ba5093476d09e1f6d5261377b289fb40fea5a4
todsah <todsah@76abf1b1-d79f-4ac6-b6dc-c8f82f358936>
2009-04-05 15:35:57 +0000

    License and Readme added.
    git-svn-id: https://svn.electricmonk.nl/svn/templum/trunk@39 76abf1b1-d79f-4ac6-b6dc-c8f82f358936

--- build-rules/release/b-run.sh
+++ build-rules/release/b-run.sh
@@ -16,11 +16,15 @@ mkdir release/templum-$VERSION
 mkdir release/templum-$VERSION/docs
 mkdir release/templum-$VERSION/docs/userguide
 mkdir release/templum-$VERSION/docs/api
-mkdir release/templum-$VERSION/templum
+mkdir release/templum-$VERSION/src
 cp docs/userguide/userguide.html release/templum-$VERSION/docs/userguide/
+cp docs/license/LICENSE.txt release/templum-$VERSION/
+cp docs/readme/README.txt release/templum-$VERSION/
+cp -ar examples release/templum-$VERSION/
 cp -r docs/api/html release/templum-$VERSION/docs/api/
-cp src/templum.php release/templum-$VERSION/templum/
+cp src/templum.php release/templum-$VERSION/src/
 cd release
+find templum-$VERSION -name '.svn' -exec rm -rf '{}' \; 2>/dev/null
 tar -czf templum-src-$VERSION.tar.gz templum-$VERSION
 cd ..
 rm -rf release/templum-$VERSION 
---
+++ docs/readme/README.txt
@@ -0,0 +1,10 @@
+Templum
+
+Templum is an extremely lightweight, simple yet powerful and fast templating
+engine for PHP. It re-uses the power of PHP itself for rendering templates, but
+provides additional features making it easier to write templating code.
+Rendering templates using Templum is very fast; it approximates native PHP
+rendering speed for include() statements.
+
+For more information, see the User Guide in docs/userguide/.
+
